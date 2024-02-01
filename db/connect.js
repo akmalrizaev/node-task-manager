d808229b90625d0cb7d817ef0291817391746c15
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-02-01 16:14:35 +0500

    Refactor Connect

--- db/connect.js
+++ db/connect.js
@@ -3,16 +3,13 @@ const mongoose = require('mongoose');
 const connectionString =
   'mongodb+srv://akmalcert:Kandym@nodetaskmanager.rkirtmg.mongodb.net/Node-Task-Manager?retryWrites=true&w=majority';
 
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to the DB...');
-  })
-  .catch((err) => {
-    console.log(err);
   });
+};
+
+module.exports = connectDB;
