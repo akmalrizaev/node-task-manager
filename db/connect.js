59dc6eeb8b6bdfc547f8847ff49c18e699296af0
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-02-01 16:34:36 +0500

    ENV VARS

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString =
-  'mongodb+srv://akmalcert:Kandym@nodetaskmanager.rkirtmg.mongodb.net/Node-Task-Manager?retryWrites=true&w=majority';
-
 const connectDB = (url) => {
-  return mongoose.connect(connectionString, {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
